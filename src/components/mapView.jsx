437dc70f977b0c00a0762202fed3481f49ac7622
camorimpe-commits <camorimpe@gmail.com>
2026-03-27 10:54:40 -0300

    fix: usar env no google maps

--- src/components/mapView.jsx
+++ src/components/mapView.jsx
@@ -12,7 +12,7 @@ const center = {
 
 export default function MapView() {
     return (
-        <LoadScript googleMapsApiKey="AIzaSyCq-iaaUTIrx6LIhCgZFNtF3jLO4jywEtU">
+        <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
             <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={5}>
                 <Marker position={{ lat: 48.8566, lng: 2.3522 }} />
             </GoogleMap>
